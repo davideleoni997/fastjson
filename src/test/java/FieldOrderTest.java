42f41c7e55d0c01c2f4321b8968e6e94ed6dca45
davideleoni997 <56631913+davideleoni997@users.noreply.github.com>
2022-05-26 17:23:24 -0400

    Removed tests

---
+++ src/test/java/FieldOrderTest.java
@@ -0,0 +1,89 @@
+import com.alibaba.fastjson.JSON;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Author : Davide Leoni
+ */
+@RunWith(value= Parameterized.class)
+public class FieldOrderTest
+{
+
+    private String personName;
+    private String schoolName;
+    private String expected;
+
+    @Parameterized.Parameters
+    public static Collection<String[]> testParameters(){
+    return Arrays.asList(new String[][]{
+            {"njb","llyz","{\"name\":\"njb\",\"school\":{\"name\":\"llyz\"}}"}, //PersonName, schoolName, expected
+
+    });
+    }
+
+    public FieldOrderTest(String personName,String schoolName,String expected){
+        configure(personName,schoolName,expected);
+    }
+
+    public void configure(String personName, String schoolName, String expected) {
+        this.personName = personName;
+        this.schoolName = schoolName;
+        this.expected = expected;
+    }
+
+    @Test
+    public void test_field_order(){
+        Person p = new Person();
+        p.setName(personName);
+        School s = new School();
+        s.setName(schoolName);
+        p.setSchool(s);
+        String json = JSON.toJSONString(p);
+        assertEquals(expected, json);
+    }
+
+    public static class Person {
+        private String name;
+        private School school;
+
+        public boolean isSchool() {
+            return false;
+        }
+
+        public School getSchool() {
+            return school;
+        }
+
+        public void setSchool(School school) {
+            this.school = school;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+    public static class School {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
---
+++ src/test/java/OrderedFieldTest.java
@@ -0,0 +1,50 @@
+import org.junit.Assert;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value= Parameterized.class)
+public class OrderedFieldTest{
+
+    private String modello;
+    private int id;
+
+    @Parameterized.Parameters
+    public static Collection<String[]> TestParameters(){
+        return Arrays.asList(new String[][] {
+
+                {"{\"id\":1001}","1001"}, //model, expectedId
+        });
+    }
+
+    public OrderedFieldTest(String modello, String id){
+        configure(modello,id);
+    }
+
+    public void configure(String modello, String id) {
+        this.modello= modello;
+        this.id= Integer.parseInt(id);
+    }
+
+    @Test
+    public void test_ordered_field() throws Exception {
+        String text = modello;
+        Model model = JSON.parseObject(text, Model.class, Feature.OrderedField);
+        Assert.assertEquals(id, model.getId());
+        String text2 = JSON.toJSONString(model);
+        Assert.assertEquals(text, text2);
+        
+    }
+    
+    public static interface Model {
+        public int getId();
+        public void setId(int value);
+    }
+}
